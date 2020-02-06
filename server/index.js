23183d882d4e8d028b035d4fcdc87dc54367a221
David Paul <davidpaul@Davids-MacBook-Pro.local>
2020-02-05 16:38:33 -0800

    proxy server now works on docker, no longer works on localhost

--- server/index.js
+++ server/index.js
@@ -9,23 +9,26 @@ app.use(express.static(path.join(__dirname, '../client/')))
 
 app.get('/carousel', (req, res) => {
     console.log('get request for carousel')
-    axios.get('http://localhost:2500/bundle.js')
+    axios.get('http://carousel:2500/bundle.js')
         .then((response) => {
             console.log('my proxy server works for carousel')
             res.send(response.data)
             res.end()
         }).catch((err) => {
-            console.log('an error occured on the get to bundle.js for carosoul')
+            console.log(err)
             res.end()
         })
 })
 
 app.get('/checkout', (req, res) => {
     console.log('get request for checkout')
-    axios.get('http://localhost:2000/bundle.js')
+    axios.get('http://checkout:2000/bundle.js')
         .then((response) => {
             console.log('my proxy server works for checkout')
             res.send(response.data)
+            res.end()
+        }).catch((err) => {
+            console.log(err)
             res.end()
         })
 
